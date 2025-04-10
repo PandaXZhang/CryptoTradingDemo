2e4b9b828b9fe12a42f8cf874969353d917cf41a
PandaXZhang <pandazhang740@gmail.com>
2025-04-10 22:12:53 +0800

    [feat]: modules separation: trading market, order and watchlist

---
+++ CryptoTradingDemo/CryptoTradingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTradingDemoApp.swift
+//  CryptoTradingDemo
+//
+//  Created by spantar on 2025/4/9.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTradingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CryptoTradingDemoTests/CryptoTradingDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTradingDemoTests.swift
+//  CryptoTradingDemoTests
+//
+//  Created by spantar on 2025/4/9.
+//
+
+import Testing
+@testable import CryptoTradingDemo
+
+struct CryptoTradingDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
